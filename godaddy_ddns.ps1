30852f551db45bbf5d945bdcb11323f82e0f4fcd
Mark A. Donohoe <Mark@MarkDonohoe.com>
2021-08-05 04:37:15 -0400

    Parameterized the API

--- godaddy_ddns.ps1
+++ godaddy_ddns.ps1
@@ -1,30 +1,34 @@
 <#
 This script is used to check and update your GoDaddy DNS server to the IP address of your current internet connection.
-
 First go to GoDaddy developer site to create a developer account and get your key and secret
-
 https://developer.godaddy.com/getstarted
  
-Update the first 4 varriables with your information
- 
-#>
-$domain = 'your.domain.to.update'  # your domain
-$name = 'name_of_host' #name of the A record to update
-$key = 'key' #key for godaddy developer API
-$secret = 'Secret' #Secret for godday developer API
+Pass in the first 4 varriables as command-line arguments, or hard-code them here if you prefer
 
-$headers = @{}
+e.g. '.\godaddy_ddns.ps1 keykeykey secretsecretsecret MarkDonohoe.com home'
+#>
+
+$key    = $args[0] # key for godaddy developer API
+$secret = $args[1] # Secret for godday developer API
+$domain = $args[2] # Your domain
+$name   = $args[3] # #name of the A record to update
+
+$endpoint                 = 'https://api.godaddy.com/v1/domains/' + $domain + '/records/A/' + $name
+$headers                  = @{}
 $headers["Authorization"] = 'sso-key ' + $key + ':' + $secret
-$result = Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -method get -headers $headers
-$content = ConvertFrom-Json $result.content
-$dnsIp = $content.data
+$result                   = Invoke-WebRequest $endpoint -method get -headers $headers -UseBasicParsing
+$content                  = ConvertFrom-Json $result.content
+$dnsIp                    = $content.data
 
 # Get public ip address there are several websites that can do this.
+# Note: 'ExpandProperty' extracts the property value from the key/value pair that Select-Object returns
 $currentIp = Invoke-RestMethod http://ipinfo.io/json | Select-Object -ExpandProperty ip
 
 if ( $currentIp -ne $dnsIp) {
-    $Request = @(@{ttl=3600;data=$currentIp; })
+
+    $Request = @(@{ ttl=600; data=$currentIp; })
     $JSON = Convertto-Json $request
-    Invoke-WebRequest https://api.godaddy.com/v1/domains/$domain/records/A/$name -method put -headers $headers -Body $json -ContentType "application/json"
+
+    Invoke-WebRequest $endpoint -method put -headers $headers -Body $json -ContentType "application/json" -UseBasicParsing
 }
 
